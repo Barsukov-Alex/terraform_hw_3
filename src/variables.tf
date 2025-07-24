7f515455ed5afbd0cc1275ae79f97e6eec94d2d3
Alex Barsukov <mozdoksity@ya.ru>
2025-07-24 09:44:29 +0300

    cделал 4 задания

--- src/variables.tf
+++ src/variables.tf
@@ -1,16 +1,14 @@
 ###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
 
 variable "cloud_id" {
   type        = string
+  default = "b1gi0o9ad7g6hm1qke89"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default = "b1g9tiefu5iru20j24oh"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -29,4 +27,30 @@ variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network&subnet name"
+}
+
+# Переменная для ВМ баз данных
+variable "vm_os_image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "Image family for the VM"
+}
+
+variable "each_vm" {
+  type = list(object({
+    vm_name       = string
+    cpu           = number
+    ram           = number
+    disk_volume   = number
+    platform_id   = string  # Добавляю платформу
+    preemptible   = bool    # Добавляю параметр preemptible
+    core_fraction = number  # Добавляю core_fraction для экономии
+    disk_type     = string  # Добавляю тип диска
+    nat           = bool    # Добавляю Nat
+  }))
+  default = [
+    { vm_name = "main", cpu = 2, ram = 2, disk_volume = 10, platform_id = "standard-v3", preemptible = true, nat = true, core_fraction = 20, disk_type = "network-hdd" },
+    { vm_name = "replica", cpu = 2, ram = 2, disk_volume = 10, platform_id = "standard-v3", preemptible = true, nat = true, core_fraction = 20, disk_type = "network-hdd" }
+  ]
+  description = "Configuration for database VMs"
 }
